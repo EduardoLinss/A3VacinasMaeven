b75133a05881679e8989a48be4b7809c188cec0e
EduardoLins <linseduardo878@gmail.com>
2023-12-05 10:59:46 -0300

    Fourth commit

---
+++ src/test/java/demo/TesteDadosDaCaderneta.java
@@ -0,0 +1,57 @@
+package demo;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import demo.Conexao.Conexao;
+import demo.entidade.caderneta;
+
+public class TesteDadosDaCaderneta {
+    
+
+
+   
+    @Test
+    public void TesteDadosDaCaderneta() throws Exception{
+        List <caderneta> list = new ArrayList<caderneta>();
+        
+        String sql = "select * from caderneta";
+
+       PreparedStatement ps = null;
+       ResultSet scann = null;
+        try{
+            if(ps == null){
+                ps = Conexao.openDatabase().prepareStatement(sql);
+                scann = ps.executeQuery();
+                while (scann.next()) {
+                    caderneta caderneta = new caderneta(0, sql, sql, sql);
+                    caderneta.setNome(scann.getString("NomeVacina"));
+                    caderneta.setDataAplic(scann.getString("dataAplic"));
+                    caderneta.setDose(scann.getString("dose"));
+                    caderneta.setLocal(scann.getString("local"));
+                    caderneta.setCidade(scann.getString("cidade"));
+                    list.add(caderneta);
+                   
+                }
+
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            ps.close();
+        }
+       
+    }
+}
+
---
+++ src/test/java/demo/TesteDadosDeRequisicao.java
@@ -0,0 +1,68 @@
+package demo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import demo.Conexao.Conexao;
+import demo.entidade.requsicao;
+
+public class TesteDadosDeRequisicao {
+    
+    String pesquisa;
+
+    /**
+     * 
+     */
+    @Test
+    public void TesteRequisicao(){
+        List <requsicao> dados = new ArrayList<requsicao>();
+        String sql = "select * from requisicao where NomePais=?"; 
+ 
+       PreparedStatement ps = null;
+       ResultSet scann = null;
+  
+       try{
+
+        if(ps == null){
+            
+            ps = Conexao.openDatabase().prepareStatement(sql);   
+            ps.setString(1, pesquisa);   
+            ps.executeQuery();
+            scann = ps.executeQuery();
+            while (scann.next()) {  
+                requsicao requsicao = new requsicao(sql, sql);
+               
+                requsicao.setId(scann.getInt("id_pais"));
+                requsicao.setnomePais(scann.getString("NomePais"));
+                requsicao.setVacinas(scann.getString("vacinas"));
+              
+    assertEquals("Australia", pesquisa);
+                dados.add(requsicao);
+            }
+        }
+       }catch (SQLException e){
+            e.printStackTrace();
+       }finally{
+        try{
+            if (scann != null){
+                scann.close();
+            }
+            if (ps != null){
+                ps.close();
+            }
+            
+        }catch(SQLException e){
+        e.printStackTrace();
+       }
+       }
+     
+    }
+}
+
